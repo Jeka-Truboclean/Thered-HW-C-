e96d6e92b4d8dcd1cde1997fcd90b8f13b0a94f3
Eugen Karaman <karamanjekakol@gmail.com>
2024-01-31 22:59:18 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Thered_HW
+{
+    class Program
+    {
+        static void Main()
+        {
+            string Str = "x w x y x w";
+            int x = 0, w = 0, y = 0;
+            for (int i = 0; i < Str.Length; i++) 
+            { if (Str[i] == 'x') { x++; } else if (Str[i] == 'w') { w++; } else if (Str[i] == 'y') { y++; } } // I could also use here Str.Contains() == true
+            Console.WriteLine($"X: {x}\nY: {y}\nW: {w}");
+        }
+    }
+}
